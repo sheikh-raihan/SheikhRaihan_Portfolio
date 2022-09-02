0962e4afba5f0aaf8dfd150fc5d0a7e2d904d136
Your Name <you@example.com>
2022-09-02 18:56:10 +0600

    Uploading Portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 150){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
